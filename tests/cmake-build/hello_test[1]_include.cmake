18282c46ce69cbf8bebc566c8dece527269894ff
jesuslovesyoutoday <73426135+jesuslovesyoutoday@users.noreply.github.com>
2022-05-05 00:05:09 +0300

    forgot cmake build here

--- tests/cmake-build/hello_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/scandiy/keplerian_to_cartesian/tests/cmake-build/hello_test[1]_tests.cmake")
-  include("/home/scandiy/keplerian_to_cartesian/tests/cmake-build/hello_test[1]_tests.cmake")
-else()
-  add_test(hello_test_NOT_BUILT hello_test_NOT_BUILT)
-endif()
